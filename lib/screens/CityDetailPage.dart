4889bed7691fdd4d738324d86886b96d78b887ec
Miraç Furkan Durmaz <117925817+mirac2534@users.noreply.github.com>
2025-04-28 17:44:20 +0300

    CityDetailPage.dart

--- lib/screens/CityDetailPage.dart
+++ lib/screens/CityDetailPage.dart
@@ -1,252 +1,243 @@
-import 'dart:convert'; // to read JSON file
+import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart'; // to access rootBundle
-import 'package:shared_preferences/shared_preferences.dart'; // SharedPreferences
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+import 'package:hava_durumu/models/wheather_model.dart';
+import 'package:hava_durumu/services/wheater_service.dart';
 import 'package:hava_durumu/models/city_images.dart';
+import 'CityDetailPage.dart';
 
-class CityDetailPage extends StatefulWidget {
-  final String city;
-
-  const CityDetailPage({Key? key, required this.city}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  _CityDetailPageState createState() => _CityDetailPageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _CityDetailPageState extends State<CityDetailPage> {
-  late List<String> allCities = [];
-  List<String> cityHistory =
-      []; // City history (to be uploaded from SharedPreferences)
-  List<String> filteredCities = []; // Search results Dec
-  final TextEditingController searchController = TextEditingController();
+class _HomePageState extends State<HomePage> {
+  String _currentTime = '';
+  Timer? _timer;
+  List<WheatherModel> _weathers = [];
+  String? _city;
+  String? _randomCityImage;
 
   @override
   void initState() {
     super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    _setRandomCityImage();
+    _startClock();
+    _initialWheatherData();
+  }
 
-    // Read the history from SharedPreferences
-    _loadCityHistory();
+  void _setRandomCityImage() {
+    final random = Random();
+    final cityKeys = cityImages.keys.toList();
+    final randomCity = cityKeys[random.nextInt(cityKeys.length)];
+    setState(() {
+      _randomCityImage = cityImages[randomCity];
+    });
+  }
 
-    // Read the city list from JSON
-    _loadCities();
-
-    // Refresh Decryption as the search text changes
-    searchController.addListener(() {
+  void _startClock() {
+    _currentTime = DateFormat('HH:mm').format(DateTime.now());
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
-        if (searchController.text.isNotEmpty) {
-          filteredCities = allCities
-              .where((city) => city
-                  .toLowerCase()
-                  .startsWith(searchController.text.toLowerCase()))
-              .toList();
-        } else {
-          filteredCities = [];
-        }
+        _currentTime = DateFormat('HH:mm').format(DateTime.now());
       });
     });
   }
 
-  Future<void> _loadCities() async {
+  Future<void> _initialWheatherData() async {
     try {
-      final String response =
-          await rootBundle.loadString('lib/models/cities.json');
-      final List<dynamic> cities = json.decode(response);
-
-      setState(() {
-        allCities = List<String>.from(cities);
-        allCities.remove(widget.city); // Remove the current city
-        filteredCities = [];
-      });
+      String? cityFromLocation = await WheaterService().getLocation();
+      _city = cityFromLocation ?? "İstanbul";
+      _weathers = await WheaterService().getWeatherData(cityName: _city);
+      setState(() {});
     } catch (e) {
-      print("Şehirler yüklenirken hata oluştu: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Konum bilgisi alınamadı: $e")),
+      );
     }
   }
 
-  Future<void> _loadCityHistory() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      cityHistory = prefs.getStringList('cityHistory') ?? [];
-    });
-  }
-
-  Future<void> _saveCityHistory() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setStringList('cityHistory', cityHistory);
-  }
-
-  void _onCitySelected(String cityName) async {
-    setState(() {
-      if (!cityHistory.contains(cityName)) {
-        // Add a new city to the city history
-        cityHistory.insert(0, cityName);
-      } else {
-        // If the current city existed in the past, first take it out, then move it to the top
-        cityHistory.remove(cityName);
-        cityHistory.insert(0, cityName);
-      }
-
-      // If there are more than 8 cities in the past, delete the oldest city (at the end)
-      if (cityHistory.length > 8) {
-        cityHistory.removeLast();
-      }
-    });
-
-    // Save updated history to SharedPreferences
-    await _saveCityHistory();
-
-    // Return the selected city
-    Navigator.pop(context, cityName);
+  Future<void> _updateWeatherForCity(String cityName) async {
+    try {
+      _city = cityName;
+      _weathers = await WheaterService().getWeatherData(cityName: _city);
+      setState(() {});
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Şehir bilgisi alınamadı: $e")),
+      );
+    }
   }
 
   @override
   void dispose() {
-    searchController.dispose();
+    _timer?.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final String? cityImage =
-        cityImages[widget.city]; // City background picture
+    final String? cityImage = cityImages[_city];
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Arama Menüsü',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.lightBlueAccent,
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: cityImage != null
-                ? AssetImage(cityImage) // If you have a city picture, show it
-                : const AssetImage(
-                    'assets/images/default.jpg'), // default picture
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Mevcut şehir başlığı
-              Text(
-                'Mevcut Şehir: ${widget.city}',
-                style: const TextStyle(
-                  fontSize: 21,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: _city == null
+                    ? (_randomCityImage != null
+                        ? AssetImage(_randomCityImage!)
+                        : const AssetImage('assets/images/default.jpg'))
+                    : (cityImage != null
+                        ? AssetImage(cityImage)
+                        : const AssetImage('assets/images/default.jpg')),
+                fit: BoxFit.cover,
               ),
-              const SizedBox(height: 20),
-              // the search bar
-              TextField(
-                controller: searchController,
-                decoration: InputDecoration(
-                  labelText: 'Şehir ara',
-                  labelStyle: const TextStyle(
-                    color: Colors.black,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: const BorderSide(
-                      color: Colors.lightBlueAccent,
-                      width: 2.0,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: const BorderSide(
-                      color: Colors.lightBlueAccent,
-                      width: 2.0,
-                    ),
-                  ),
-                  prefixIcon: const Icon(
-                    Icons.search,
-                    color: Colors.lightBlueAccent,
-                  ),
-                ),
-                style: const TextStyle(
-                  color: Colors.black,
-                ),
-              ),
-
-              const SizedBox(height: 20),
-              // City history title
-              const Text(
-                "Şehir geçmişi:",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-              // List of city history
-              Expanded(
-                child: cityHistory
-                        .isEmpty // If you have never searched for a city, this bar will be empty
-                    ? const Text("Henüz bir şehir aratmadın.")
-                    : ListView.builder(
-                        itemCount: cityHistory.length,
-                        itemBuilder: (context, index) {
-                          final cityName = cityHistory[index];
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 0.5),
-                            child: ElevatedButton(
-                              onPressed: () {
-                                Navigator.pop(context, cityName);
-                              },
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.white,
-                              ),
-                              child: Text(
-                                cityName,
-                                style: const TextStyle(color: Colors.black),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-              ),
-              const SizedBox(height: 20),
-              // If the search bar is not empty, the list of cities
-              if (searchController.text.isNotEmpty)
-                const Text(
-                  "Şehirler:",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              if (searchController.text.isNotEmpty) const SizedBox(height: 10),
-              if (searchController.text.isNotEmpty)
-                Expanded(
-                  child: filteredCities.isEmpty
-                      ? const Center(child: Text("Şehir bulunamadı"))
-                      : ListView.builder(
-                          itemCount: filteredCities.length,
-                          itemBuilder: (context, index) {
-                            final cityName = filteredCities[index];
-                            return Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(vertical: 4.0),
-                              child: ElevatedButton(
-                                onPressed: () => _onCitySelected(cityName),
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.blue.shade100,
-                                ),
-                                child: Text(
-                                  cityName,
-                                  style: const TextStyle(color: Colors.black),
-                                ),
-                              ),
-                            );
-                          },
+            ),
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                if (_city != null)
+                  GestureDetector(
+                    onTap: () async {
+                      final selectedCity = await Navigator.push<String>(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => CityDetailPage(city: _city!),
                         ),
+                      );
+                      if (selectedCity != null && selectedCity.isNotEmpty) {
+                        await _updateWeatherForCity(selectedCity);
+                      }
+                    },
+                    child: Text(
+                      _city!,
+                      style: const TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                if (_city == null) const SizedBox(height: 28),
+                const SizedBox(height: 20),
+                Expanded(
+                  child: ListView.builder(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    itemCount: _weathers.length,
+                    itemBuilder: (context, index) {
+                      final weather = _weathers[index];
+
+                      int newDerece2 = double.parse(weather.derece).round();
+                      int newMax2 = double.parse(weather.max).round();
+                      int newMin2 = double.parse(weather.min).round();
+                      int newGece2 = double.parse(weather.gece).round();
+                      int newNem2 = double.parse(weather.nem).round();
+
+                      String capitalize(String text) {
+                        if (text.isEmpty) return text;
+                        return text[0].toUpperCase() +
+                            text.substring(1).toLowerCase();
+                      }
+
+                      return Card(
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        color: Colors.white.withOpacity(0.8),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                weather.gun,
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              Row(
+                                children: [
+                                  Image.network(
+                                    weather.ikon,
+                                    width: 40,
+                                    height: 40,
+                                    errorBuilder:
+                                        (context, error, stackTrace) =>
+                                            const Icon(Icons.broken_image,
+                                                size: 40),
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Expanded(
+                                    child: Text(
+                                      capitalize(weather.durum),
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.grey.shade700,
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    '${newDerece2}°C',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.grey.shade700,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 10),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text('Max: ${newMax2}°C',
+                                      style: const TextStyle(
+                                          fontSize: 14, color: Colors.black87)),
+                                  Text('Min: ${newMin2}°C',
+                                      style: const TextStyle(
+                                          fontSize: 14, color: Colors.black87)),
+                                  Text('Gece: ${newGece2}°C',
+                                      style: const TextStyle(
+                                          fontSize: 14, color: Colors.black87)),
+                                  Text('Nem: ${newNem2}%',
+                                      style: const TextStyle(
+                                          fontSize: 14, color: Colors.black87)),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ),
-            ],
+              ],
+            ),
           ),
-        ),
+          Positioned(
+            top: 20,
+            left: 20,
+            child: Text(
+              _currentTime,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
